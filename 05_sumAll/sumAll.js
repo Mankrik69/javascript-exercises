a12cc118aa018468ccd44f0281fb71991e3cf7ca
Mankrik69 <mankrik69@gmail.com>
2024-01-22 13:24:03 +0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (typeof start !== "number" ||
+        typeof end !== "number" ||
+        start < 0 ||
+        end < 0) return "ERROR";
+    let sum = 0;
 
+    if (start > end) {
+        for (start; start >= end; start--) sum += start;
+    }
+    else {
+        for (start; start <= end; start++) sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
