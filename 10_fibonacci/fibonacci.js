5ddc6431db998a991ab24d2b8e70de478f25bd1b
Mankrik69 <mankrik69@gmail.com>
2024-01-30 08:51:35 +0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
 
+    let current = 0;
+    let next = 1;
+
+    for (let i = 0; i < n; i++) {
+        let tmp = next
+        next += current;
+        current = tmp;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
