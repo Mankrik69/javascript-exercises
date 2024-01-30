1887e4a4d3e8e0a16809a6ba48846c3a25a0385a
Mankrik69 <mankrik69@gmail.com>
2024-01-30 09:45:00 +0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        if (!oldest.yearOfDeath) oldest.yearOfDeath = new Date().getFullYear();
+        if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
 
+        if (person.yearOfDeath - person.yearOfBirth >
+            oldest.yearOfDeath - oldest.yearOfBirth) {
+            oldest = person;
+        }
+
+        return oldest;
+    });
 };
 
 // Do not edit below this line
