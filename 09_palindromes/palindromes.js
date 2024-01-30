596aba9e0a150cdc584e7f0afb92e00a15cc40ce
Mankrik69 <mankrik69@gmail.com>
2024-01-30 08:30:53 +0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const delim = [" ", ",", ".", "!"];
+    str = str.toLowerCase();
 
+    for (char of delim) {
+        if (str.includes(char)) {
+            str = str.split(char);
+            str = str.join("");
+        }
+    }
+
+    return str === str.split("").reverse().join("");
 };
 
 // Do not edit below this line
